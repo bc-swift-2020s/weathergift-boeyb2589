0ef80f7bcd2eaab3658824bd29429f2605e7b564
Brandon Boey <brandonjboey@gmail.com>
2020-03-08 15:39:53 -0400

    CH 7 2

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Brandon Boey on 3/8/20.
+//  Copyright © 2020 Brandon Boey. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
