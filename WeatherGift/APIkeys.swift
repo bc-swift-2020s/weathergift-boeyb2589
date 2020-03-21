de6397ff89f037da7643196ece69f7003c203a13
Brandon Boey <brandonjboey@gmail.com>
2020-03-21 19:01:26 -0400

    with .gitignore

---
+++ WeatherGift/APIkeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIkeys.swift
+//  WeatherGift
+//
+//  Created by Brandon Boey on 3/21/20.
+//  Copyright © 2020 Brandon Boey. All rights reserved.
+//
+
+import Foundation
+
+struct APIkeys {
+    static let googlePlacesKey = "AIzaSyA3U0-8q-fZacTyWuGOcaeYlokQQyTCfu8"
+}
